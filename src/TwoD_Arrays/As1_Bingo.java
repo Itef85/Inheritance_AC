0eb51ec82b639c6170bca846fb8f78bcdb05d5be
a.cordell <atticordell85@gmail.com>
2025-02-03 11:39:59 -0700

    Bingo part 80*

--- src/TwoD_Arrays/As1_Bingo.java
+++ src/TwoD_Arrays/As1_Bingo.java
@@ -7,8 +7,10 @@ public class As1_Bingo {
     public static void run(){
 
         char[] Titles = { 'B','I','N','G','O'};
+        int num;
 
         int[][] BingoCard = {
+
                 generateArray(5, 1, 15),
                 generateArray(5, 16, 30),
                 generateArray(5, 31, 45),
@@ -17,15 +19,38 @@ public class As1_Bingo {
         };
 
 
-        printBingo(BingoCard);
+        printBingo(BingoCard, Titles);
+        while(true){
+
+           String intput = Library.input.nextLine();
+           if ( intput == ""){
+               System.out.println();
+
+               num =  Library.myRandom(1,75);
+
+               for (int i = 0; i < BingoCard.length; i++) {
+
+
+
+               }
+
+
+
+               printBingo(BingoCard, Titles);
+               System.out.println(num);
+           }
+
+
+        }
     }//run
-    public static void printBingo(int[][] grid) {
+    public static void printBingo(int[][] grid, char[] Titles) {
         for (int row = 0; row < 5; row++) {
+            System.out.print(Titles[row] + " ");
             for (int col = 0; col < 5; col++) {
                 if (   grid[row][col] == 0  ) {
-                    System.out.print("  ");
+                    System.out.print(" ");
                 }else{
-                    System.out.print(grid[row][col] + " ");
+                    System.out.print(grid[row][col] + "  ");
                 }
             } // col
             System.out.println();
